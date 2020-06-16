b17cc5e54c82685c4752ec3f5cda080e7bd68c50
amehure <47616477+amehure@users.noreply.github.com>
2020-06-17 00:09:46 +0900

    improved create_or_delete_user

--- db/migrate/20200616051329_rename_event_sub_no_clomun_to_dday.rb
+++ db/migrate/20200616051329_rename_event_sub_no_clomun_to_dday.rb
@@ -1,7 +1,10 @@
 class RenameEventSubNoClomunToDday < ActiveRecord::Migration[6.0]
   def change
-    change_column :ddays, :event_sub_no, :integer
-    change_column :users, :event_sub_no, :integer
-    change_column :users, :serial_no, :integer
+    remove_column :ddays, :event_sub_no
+    remove_column :users, :event_sub_no
+    remove_column :users, :serial_no
+    add_column :ddays, :event_sub_no, :integer
+    add_column :users, :event_sub_no, :integer
+    add_column :users, :serial_no, :integer
   end
 end
